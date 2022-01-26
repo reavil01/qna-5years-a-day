8803bbf15d4cc99b497a442b0e7483aeacd26923
reavil01 <kdbml314@gmail.com>
2022-01-26 15:18:39 +0900

    Test: add null test and replace test code using mock
    - replace question repository instance to mock
    - remove spring code for testing

--- src/test/kotlin/com/yearsaday/qna/service/QuestionDataServiceTest.kt
+++ src/test/kotlin/com/yearsaday/qna/service/QuestionDataServiceTest.kt
@@ -1,99 +1,178 @@
 package com.yearsaday.qna.service
 
 import com.yearsaday.qna.message.QuestionRequest
+import com.yearsaday.qna.message.QuestionResponse
 import com.yearsaday.qna.repository.QuestionDataService
+import com.yearsaday.qna.spring.entity.QuestionEntity
 import com.yearsaday.qna.spring.repository.QuestionRepository
+import com.yearsaday.qna.spring.service.QuestionSpringDataService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.assertThrows
+import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.verify
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import java.lang.NullPointerException
 import java.time.LocalDateTime
+import java.util.*
 
-@SpringBootTest
 class QuestionDataServiceTest {
-    @Autowired
     private lateinit var service: QuestionDataService
 
-    @Autowired
+    @Mock
     private lateinit var repository: QuestionRepository
 
-    val SENTENCE = "질문1"
+    private val request = makeQuestionRequest()
+    private val entity = request.toEntity()
+    private val response = entity.toResponse()
 
     @BeforeEach
-    fun cleanUp() {
-        repository.deleteAll()
+    fun init() {
+        MockitoAnnotations.openMocks(this)
+        service = QuestionSpringDataService(repository)
     }
 
     @Test
     fun saveQuestionTest() {
         // given
-        val request = makeQuestionRequest()
+        given(repository.save(entity))
+            .willReturn(entity)
 
         // when
         val result = service.save(request)
 
         // then
-        assertThat(repository.findAll().size).isEqualTo(1)
-        assertThat(result.id).isGreaterThan(0)
-        assertThat(result.sentence).isEqualTo(SENTENCE)
+        verify(repository).save(entity)
+        assertThat(result).isEqualTo(response)
     }
 
     @Test
     fun findQuestionTest() {
         // given
-        val request = makeQuestionRequest()
-        val saved = service.save(request)
-        assertThat(repository.findAll().size).isEqualTo(1)
+        val id = 1
+        given(repository.findById(id))
+            .willReturn(Optional.of(entity))
 
         // when
-        val result = service.select(saved.id)
+        val result = service.select(id)
 
         // then
-        assertThat(result.id).isEqualTo(saved.id)
-        assertThat(result.sentence).isEqualTo(request.sentence)
+        verify(repository).findById(id)
+        assertThat(result).isEqualTo(response)
+    }
+
+    @Test
+    fun findQuestionWhenQuestionIsNotExist() {
+        assertThrows<NoSuchElementException> {
+            // given
+            val id = 1
+            given(repository.findById(id))
+                .willReturn(Optional.empty())
+
+            // when
+            service.select(id)
+        }
     }
 
     @Test
     fun updateQuestionTest() {
         // given
-        val request = makeQuestionRequest()
-        val saved = service.save(request)
-        assertThat(repository.findAll().size).isEqualTo(1)
-        val updateSentence = "질문2"
         val updateRequest = QuestionRequest(
-            updateSentence,
+            "수정",
             LocalDateTime.now().monthValue,
             LocalDateTime.now().dayOfMonth
         )
+        val updatedEntity = updateRequest.toEntity()
+        val updateResponse = updatedEntity.toResponse()
+
+        given(repository.findById(entity.id))
+            .willReturn(Optional.of(entity))
+        given(repository.save(updatedEntity))
+            .willReturn(updatedEntity)
 
         // when
-        val result = service.update(saved.id, updateRequest)
+        val result = service.update(entity.id, updateRequest)
 
         // then
-        assertThat(repository.findAll().size).isEqualTo(1)
-        assertThat(result.sentence).isEqualTo(updateSentence)
+        verify(repository).findById(entity.id)
+        verify(repository).save(updatedEntity)
+        assertThat(result).isEqualTo(updateResponse)
+    }
+
+    @Test
+    fun updateQuestionWhenPreviousQuestionIsNotExist() {
+        assertThrows<NoSuchElementException> {
+            // given
+            val updateRequest = QuestionRequest(
+                "수정",
+                LocalDateTime.now().monthValue,
+                LocalDateTime.now().dayOfMonth
+            )
+
+            given(repository.findById(entity.id))
+                .willReturn(Optional.empty())
+
+            // when
+            service.update(entity.id, updateRequest)
+        }
     }
 
     @Test
     fun deleteQuestionTest() {
         // given
-        val question = makeQuestionRequest()
-        val saved = service.save(question)
-        assertThat(repository.findAll().size).isEqualTo(1)
 
         // when
-        service.delete(saved.id)
+        service.delete(entity.id)
 
         // then
-        assertThat(repository.findAll().size).isEqualTo(0)
+        verify(repository).deleteById(entity.id)
+    }
+
+    @Test
+    fun getTodayQuestionTest() {
+        // given
+        given(repository.findByMonthAndDay(request.month, request.day))
+            .willReturn(entity)
+
+        // when
+        val result = service.getTodayQuestion()
+
+        // then
+        verify(repository).findByMonthAndDay(request.month, request.day)
+        assertThat(result).isEqualTo(response)
+    }
+
+    @Test
+    fun getTodayQuestionWhenQuestionIsNotExist() {
+        assertThrows<NullPointerException> {
+            // given
+            given(repository.findByMonthAndDay(request.month, request.day))
+                .willReturn(null)
+
+            // when
+            service.getTodayQuestion()
+        }
     }
 
     private fun makeQuestionRequest(): QuestionRequest {
         return QuestionRequest(
-            SENTENCE,
+            "질문1",
             LocalDateTime.now().monthValue,
-            LocalDateTime.now().dayOfMonth
+            LocalDateTime.now().dayOfMonth,
         )
     }
+
+    fun QuestionRequest.toEntity() = QuestionEntity(
+        0,
+        this.sentence,
+        this.month,
+        this.day,
+    )
+
+    fun QuestionEntity.toResponse() = QuestionResponse(
+        this.id,
+        this.sentence,
+    )
 }
