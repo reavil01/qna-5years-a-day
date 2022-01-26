4f55b06f3e7136c2e3313a64779e81451d6e207a
reavil01 <kdbml314@gmail.com>
2022-01-26 16:05:02 +0900

    Test: remove code for non-defined function
    - remove test function list
     ㄴ findTest()
     ㄴ createTest()
     ㄴ updateTest()
     ㄴ deleteTest()

--- src/test/kotlin/com/yearsaday/qna/controller/QuestionControllerTest.kt
+++ src/test/kotlin/com/yearsaday/qna/controller/QuestionControllerTest.kt
@@ -1,123 +1,35 @@
 package com.yearsaday.qna.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.yearsaday.qna.message.QuestionRequest
-import com.yearsaday.qna.message.QuestionResponse
 import com.yearsaday.qna.repository.QuestionDataService
 import com.yearsaday.qna.spring.controller.QuestionSpringController
-import org.hamcrest.Matchers.containsString
 import org.junit.jupiter.api.Test
-import org.mockito.BDDMockito.given
 import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
-import java.time.LocalDateTime
 
 @WebMvcTest(QuestionSpringController::class)
 class QuestionControllerTest {
     @Autowired
     private lateinit var mock: MockMvc
 
-    @Autowired
-    private lateinit var objectMapper: ObjectMapper
-
     @MockBean
     private lateinit var questionService: QuestionDataService
 
     private val API_URL = "/questions"
-    private val QUESTION_ID = 1
-    private val QUESTION_SENTENCE = "질문1"
 
     @Test
-    fun findTest() {
-        // given
-        val questionResponse = getQuestionResponse(QUESTION_ID, QUESTION_SENTENCE)
-
-        given(questionService.select(QUESTION_ID))
-            .willReturn(questionResponse)
-
-        // when
-        mock.perform(get("$API_URL/$QUESTION_ID"))
-            .andExpect(status().isOk)
-            .andExpect(content().string(containsString(QUESTION_SENTENCE)))
-
-        // then
-        verify(questionService).select(QUESTION_ID)
-    }
-
-    @Test
-    fun createTest() {
-        // given
-        val questionCreateRequest = QuestionRequest(
-            QUESTION_SENTENCE,
-            LocalDateTime.now().monthValue,
-            LocalDateTime.now().dayOfMonth
-        )
-        val json = objectMapper.writeValueAsString(questionCreateRequest)
-
-        val questionResponse = getQuestionResponse(QUESTION_ID, QUESTION_SENTENCE)
-        given(questionService.save(questionCreateRequest))
-            .willReturn(questionResponse)
-
-        // when
-        mock.perform(
-            post(API_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json)
-        ).andExpect(status().isCreated)
-            .andExpect(content().string(containsString(QUESTION_SENTENCE)))
-
-        // then
-        verify(questionService).save(questionCreateRequest)
-    }
-
-    @Test
-    fun updateTest() {
-        // given
-        val questionUpdateRequest = QuestionRequest(
-            QUESTION_SENTENCE,
-            LocalDateTime.now().monthValue,
-            LocalDateTime.now().dayOfMonth
-        )
-        val json = objectMapper.writeValueAsString(questionUpdateRequest)
-
-        val questionResponse = getQuestionResponse(QUESTION_ID, QUESTION_SENTENCE)
-        given(questionService.update(QUESTION_ID, questionUpdateRequest))
-            .willReturn(questionResponse)
-
-        // when
-        mock.perform(
-            put("$API_URL/$QUESTION_ID")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json)
-        ).andExpect(status().isOk)
-            .andExpect(content().string(containsString(QUESTION_SENTENCE)))
-
-        // then
-        verify(questionService).update(QUESTION_ID, questionUpdateRequest)
-    }
-
-    @Test
-    fun deleteTest() {
+    fun getTodayQuestion() {
         // given
 
         // when
-        mock.perform(delete("$API_URL/$QUESTION_ID"))
+        mock.perform(get("$API_URL/"))
             .andExpect(status().isOk)
 
         // then
-        verify(questionService).delete(QUESTION_ID)
-    }
-
-    private fun getQuestionResponse(
-        questionId: Int,
-        questionSentence: String
-    ): QuestionResponse {
-        return QuestionResponse(questionId, questionSentence)
+        verify(questionService).getTodayQuestion()
     }
 }
