811c11d685311a39f20323c71166a2456f29360e
reavil01 <kdbml314@gmail.com>
2022-02-06 11:16:11 +0900

    feat: read questions.txt via resourceLoader

--- src/main/kotlin/com/yearsaday/qna/service/QuestionInputService.kt
+++ src/main/kotlin/com/yearsaday/qna/service/QuestionInputService.kt
@@ -4,21 +4,23 @@ import com.yearsaday.qna.message.QuestionRequest
 import com.yearsaday.qna.repository.QuestionDataService
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
+import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Component
 import java.io.FileReader
 
 @Component
 private class QuestionInputService(
-    val questionDataService: QuestionDataService
+    val questionDataService: QuestionDataService,
+    val resourceLoader: ResourceLoader
 ) {
     val monthList = (1..12)
     val maxDayList = listOf(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
     @EventListener(ApplicationReadyEvent::class)
     fun insertQuestionsToDB() {
-        val fileName = "questions.txt"
-        val questions = FileReader(fileName).readLines()
-
+        val resource = resourceLoader.getResource("classpath:questions.txt").file
+        val questions = FileReader(resource).readLines()
+        println(questions)
         monthList.zip(maxDayList).flatMap { (month, maxDay) ->
             (1..maxDay).map { month to it }
         }.forEachIndexed { index, (month, day) ->
